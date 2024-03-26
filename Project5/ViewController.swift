04055c485d8e4c22db8962479f71d3d2143b2c31
Anouar <anouarelmaaroufi@gmail.com>
2024-03-26 03:36:02 +0000

    Added some improvement checks to the app game
    Input is empty or starter word is the same as input word, or input word is less than three letters

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -30,9 +30,10 @@ class ViewController: UITableViewController {
         // start game
         startGame()
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
     }
 
-    func startGame() {
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
@@ -58,14 +59,28 @@ class ViewController: UITableViewController {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
                     usedWords.insert(lowerAnswer, at: 0)
-                    
+
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
+                } else {
+                    showErrorMessage("Word not recognized", "You can't just make them up, you know!")
                 }
+            } else {
+                showErrorMessage("Word used already", "Be more original!")
             }
+        } else {
+            showErrorMessage("Word not possible", "You can't spell that word from \(title!)")
         }
     }
-    
+
+    func showErrorMessage(_ title: String, _ message: String) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        
+        present(ac, animated: true)
+    }
+
     func isPossible(word: String) -> Bool {
         guard var tempWord = title?.lowercased() else { return false }
         
@@ -85,13 +100,30 @@ class ViewController: UITableViewController {
     }
 
     func isReal(word: String) -> Bool {
-        return true
+        if word.isEmpty {
+            showErrorMessage("Empty 🥹", "Write a word")
+            return false
+        }
+        if word.count < 3 {
+            showErrorMessage("Less then three letters", "Try writing a word more then three letters :)")
+            return false
+        }
+        if title == word {
+            showErrorMessage("The same as the starter word", "Write other things then the starter word in the title")
+            return false
+        }
+        
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return usedWords.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
         
